8975e2f4fe1687cb933e323797c0e9f3c104053d
Dmitriy Fedorov <dmitriyfedorovpy@yahoo.com>
2024-10-28 16:18:10 +0500

    Add files via upload

--- Лабараторная работа №1_Задание_№1.py
+++ Лабараторная работа №1_Задание_№1.py
@@ -4,16 +4,23 @@
 import math
 
 def equation_1(): #1 способ
-    x = int(input('Введите x\n')) #можно без переноса строки, но мне кажется, что так красивее
-    y = int(input('Введите y\n')) #можно инпутки засунуть в парамерты, но мне кажется, что это некрасиво
+    try:
+        x = int(input('Введите x\n')) #можно без переноса строки, но мне кажется, что так красивее
+        y = int(input('Введите y\n')) #можно инпутки засунуть в парамерты, но мне кажется, что это некрасиво
 
-    d = (2*math.cos(x - math.pi/6))/(1/2 + math.sin(y)**2) + (abs(y - x))/3
-    return d
+        d = (2*math.cos(x - math.pi/6))/(1/2 + math.sin(y)**2) + (abs(y - x))/3
+        return d
+    except ValueError:
+        print('Для получения результата нужно ввести переменную x и переменную y. '
+              'Переменные должны быть цифрами или числами.')
 
 
-def equation_2(x,y): #2 способ
+
+
+def equation_2(x,y): #2 способ|||не понял как реализовать try
     d = (2 * math.cos(x - math.pi / 6)) / (1 / 2 + math.sin(y) ** 2) + (abs(y - x)) / 3
     return d
 
+
 print(equation_1())
-print(equation_2(2,1))
+print(equation_2(2,3))
